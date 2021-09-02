1209f3dc256ef07d4b34b7cca3c883b1c03cb040
Sungoh Park <spark449062@gmail.com>
2021-09-02 09:58:24 -0600

    commit tree 9f2a9d9a0516e676aa968a3f90808b0d02e285c5

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+def line(array)
+    if array.length == 0
+        puts "The line is currently empty."
+    else
+        string = "The line is currently: "
+        for i in 0..(array.length - 1)
+            string += "#{i+1}. #{array[i]} "
+        end
+        puts string.strip
+    end
+end
+
+def take_a_number(array, element)
+    array << element
+    puts "Welcome, #{element}. You are number #{array.index(element) + 1} in line."
+end
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{array[0]}."
+        array.shift
+    end
+end
+
